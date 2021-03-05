bb5bac5186e608879d511d5cb728f096aed17a19
Usama Rehan <zrehan286@gmail.com>
2021-03-05 19:20:48 +0530

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -1,1 +1,14 @@
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+const NotFound = () => {
+  const router = useRouter();
+  
+  return (
+    <div>ID Page</div>
+  );
+  
+}
+
+export default NotFound;
