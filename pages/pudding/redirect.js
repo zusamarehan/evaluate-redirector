2aefa43a0a5ceaf6d06e031f60448e034403afa0
Usama Rehan <zrehan286@gmail.com>
2021-03-23 19:01:42 +0530

    Update redirect.js

--- pages/pudding/redirect.js
+++ pages/pudding/redirect.js
@@ -1,1 +1,19 @@
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+const PuddingRedirect = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  
+  useEffect(() => {
+    router.push('https://apps.pudding.test/asdfcallback/run') 
+  }, [id])
+  
+  return (
+    <div>Loading quiz...</div>
+  );
+  
+}
+
+export default PuddingRedirect;
